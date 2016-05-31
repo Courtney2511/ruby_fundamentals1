3d291da73c1ed5e9f19ad3a08da27cf3ce7843f3
Courtney Noonan <courtneynoonan@me.com>
2016-05-31 13:38:53 -0400

    created and tested Bitmaker Labs version of FizzBuzz

---
+++ exercise4.rb
@@ -0,0 +1,21 @@
+num = 0
+
+loop do
+  num += 1
+
+  num += 1
+
+  if ((num%3 == 0) && (num%5 == 0)) == true
+    puts "BitMaker"
+  elsif (num%3) == 0
+      puts "Bit"
+  elsif (num%5) == 0
+      puts "Maker"
+  else
+    puts num
+  end
+
+  if num == 100
+    break
+  end
+end
