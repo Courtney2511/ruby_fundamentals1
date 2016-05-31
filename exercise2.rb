33f121869915fd9c64d0efed69d87034b46404b3
Courtney Noonan <courtneynoonan@me.com>
2016-05-31 10:07:46 -0400

    exercise 2 completed

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+
+#tip on a $55 meal
+puts 55 *1.2
+
+#add an integer to a string
+puts "Courtney" + 5.to_s
+
+#use string interpolation
+puts "There are #{45628 * 7839} stars in the sky"
+
+#working with booleans
+puts (10 < 20 && 30 < 20) || !(10==11)
