fb4663b37bf61aaef2b3b819a60937794d888c1d
Courtney Noonan <courtneynoonan@me.com>
2016-05-31 21:35:41 -0400

    removed commenting

--- exercise4.rb
+++ exercise4.rb
@@ -1,23 +1,3 @@
-num = 0
-
-# loop do
-#   num += 1
-#
-#   if ((num%3 == 0) && (num%5 == 0)) == true
-#     puts "BitMaker"
-#   elsif (num%3) == 0
-#       puts "Bit"
-#   elsif (num%5) == 0
-#       puts "Maker"
-#   else
-#     puts num
-#   end
-#
-#   if num == 100
-#     break
-#   end
-# end
-
 (1..100).each do |number|
 
   if ((number % 3 == 0) && (number % 5 == 0))
